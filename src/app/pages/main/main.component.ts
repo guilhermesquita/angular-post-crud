9ecc458425b48a5ba0000234ac731ee595246084
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-05-03 18:16:07 -0400

    feat: add style to structure

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  @Input() contentBtn: string = "";
 }
