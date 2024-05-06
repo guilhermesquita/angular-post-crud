f1b1797d25017376107ecdd18d8b6d987cc65a74
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-05-06 12:10:41 -0400

    feat: create component for button

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { ButtonMainComponent } from '../button-main/button-main.component';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonMainComponent],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
